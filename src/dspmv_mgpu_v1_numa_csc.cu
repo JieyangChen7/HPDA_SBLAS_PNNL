5878ac0b6be2611e573ed9138e0d4c8f178ccccc
Jieyang Chen <cjy7117@gmail.com>
2020-01-25 23:59:19 -0500

    Update dspmv_mgpu_v1_numa_csc.cu

--- src/dspmv_mgpu_v1_numa_csc.cu
+++ src/dspmv_mgpu_v1_numa_csc.cu
@@ -395,7 +395,7 @@ spmv_ret spMV_mgpu_v1_numa_csc(int m, int n, long long nnz, double * alpha,
                     pcscGPU[dev_id].m * sizeof(double), cudaMemcpyDeviceToHost, stream)); 
 
     checkCudaErrors(cudaDeviceSynchronize());
-    print_vec_gpu(pcscGPU[dev_id].y, m, "y"+to_string(dev_id));
+    print_vec(pcscGPU[dev_id].y, m, "y"+to_string(dev_id));
     #pragma omp barrier
     if (dev_id == 0) {
       for (int i = 0; i < m; i++) {
