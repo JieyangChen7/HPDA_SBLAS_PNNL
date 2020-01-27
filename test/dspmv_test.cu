96b60572f5361d501bf7ae8e3366d0048fa2d9cc
Jieyang Chen <cjy7117@gmail.com>
2020-01-26 22:27:33 -0500

    Update dspmv_test.cu

--- test/dspmv_test.cu
+++ test/dspmv_test.cu
@@ -453,14 +453,14 @@ int main(int argc, char *argv[]) {
                                 ngpu);
     ret_baseline_csc.add(ret);
 
-    ret = spMV_mgpu_v1_numa_csc(m, n, nnz, &ALPHA,
-                                cscVal, cscColPtr, cscRowIdx,
-                                x, &BETA,
-                                y_static_csc,
-                                ngpu,
-                                1,
-                                numa_mapping); //kernel 1
-    ret_static_csc.add(ret);
+    // ret = spMV_mgpu_v1_numa_csc(m, n, nnz, &ALPHA,
+    //                             cscVal, cscColPtr, cscRowIdx,
+    //                             x, &BETA,
+    //                             y_static_csc,
+    //                             ngpu,
+    //                             1,
+    //                             numa_mapping); //kernel 1
+    // ret_static_csc.add(ret);
 
     // ret = spMV_mgpu_baseline_coo(m, n, nnz, &ALPHA,
     //                             cooVal, cooRowIdx, cooColIdx, 
