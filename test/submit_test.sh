5565308cc41ac0567cee999a7681e806d29fd5e0
Jieyang Chen <cjy7117@gmail.com>
2020-01-27 21:59:50 -0500

    Update submit_test.sh

--- test/submit_test.sh
+++ test/submit_test.sh
@@ -3,7 +3,7 @@
 job_script_template=matrix_test.sh
 #job_script=matrix_test_tmp.sh 
 
-for matrix_file in HV15R.mtx #europe_osm.mtx #road_usa.mtx #taly_osm.mtx #great-britain_osm.mtx delaunay_n22.mtx germany_osm.mtx asia_osm.mtx road_central.mtx delaunay_n23.mtx road_usa.mtx kron_g500-logn20.mtx delaunay_n24.mtx
+for matrix_file in $1
 do
   for NGPU in 1 2 3 4 5 6
   do
