8eb737a58950ca18db5dd4a4e846b31fb5cf3fb5
Jieyang Chen <cjy7117@gmail.com>
2020-01-28 17:22:25 -0500

    test

--- test/batch_test_dgx1.sh
+++ test/batch_test_dgx1.sh
@@ -2,7 +2,7 @@
 
 #all=Stanford IMDB wiki-Talk web-Google connectus NotreDame_actors citationCiteseer soc-sign-epinions human_gene2 mouse_gene hollywood-2009 com-Orkut cit-Patents com-LiveJournal wb-edu
 #large=cit-Patents com-LiveJournal com-Orkut hollywood-2009 human_gene2 mouse_gene wb-edu
-for matrix in com-Orkut
+for matrix in mouse_gene
 do
   ./submit_test_dgx1.sh ${matrix}.mtx 
 done
