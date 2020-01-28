535ec87254bf88c71e427bddcc854891fa3d5c85
Jieyang Chen <cjy7117@gmail.com>
2020-01-28 12:17:40 -0800

    update batch test

--- test/batch_test.sh
+++ test/batch_test.sh
@@ -1,15 +1,7 @@
 #/bin/bash
 
-NGPU=1
-NTEST=5
 
-_CSV_SUMMARY="summary_results_generated_matrix.csv"
-[ -f ${_CSV_SUMMARY} ] && rm ${_CSV_SUMMARY}
-for size in 10000 20000 40000
+for matrix in Stanford IMDB wiki-Talk web-Google connectus NotreDame_actors citationCiteseer soc-sign-epinions human_gene2 mouse_gene hollywood-2009 com-Orkut cit-Patents com-LiveJournal wb-edu
 do
-  _CSV_OUTPUT=generated_matrix_${size}.csv
-  [ -f ${_CSV_OUTPUT} ] && rm ${_CSV_OUTPUT}
-  ./test_spmv g $size $NGPU $NTEST generated_matrix
-  cat ${_CSV_OUTPUT} >> ${_CSV_SUMMARY}
-  echo "" >> ${_CSV_SUMMARY}
+  ./submit_test.sh ${matrix}.mtx 
 done
