ecead6987bc6671257568574b1fde088d3c9be0b
Jieyang Chen <cjy7117@gmail.com>
2020-01-29 16:43:05 -0500

    test

--- test/dspmv_test_dgx1.cu
+++ test/dspmv_test_dgx1.cu
@@ -97,11 +97,11 @@ int main(int argc, char *argv[]) {
   //int copy_of_workspace = atoi(argv[8]);
 
 
-  double ** fake_data = new double*[500];
-  for (int i = 0; i < 500; i++) {
-    checkCudaErrors(cudaMallocHost((void **)&(fake_data[i]), 134217728 * sizeof(double)));
-    cout << "allocated pin memory: " << i << " GB" << endl;
-  }
+  // double ** fake_data = new double*[500];
+  // for (int i = 0; i < 500; i++) {
+  //   checkCudaErrors(cudaMallocHost((void **)&(fake_data[i]), 134217728 * sizeof(double)));
+  //   cout << "allocated pin memory: " << i << " GB" << endl;
+  // }
 
   report_all_mem_usage();
 
