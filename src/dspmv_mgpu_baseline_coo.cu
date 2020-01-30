9300bcc100419684c7d0f0605d054d59a48f6a1b
Jieyang Chen <cjy7117@gmail.com>
2020-01-30 16:55:59 -0500

    Update dspmv_mgpu_baseline_coo.cu

--- src/dspmv_mgpu_baseline_coo.cu
+++ src/dspmv_mgpu_baseline_coo.cu
@@ -146,9 +146,9 @@ spmv_ret spMV_mgpu_baseline_coo(int m, int n, int nnz, double * alpha,
   for (int d = 0; d < ngpu; ++d) {
     checkCudaErrors(cudaSetDevice(d));
     
-    // coo2csrGPU(handle[d], stream[d], dev_m[d], dev_n[d], dev_nnz[d],
-    //             dev_cooVal[d], dev_cooRowIdx[d], dev_cooColIdx[d],
-    //             dev_csrVal[d], dev_csrRowPtr[d], dev_csrColIdx[d]);
+    coo2csrGPU(handle[d], stream[d], dev_m[d], dev_n[d], dev_nnz[d],
+                dev_cooVal[d], dev_cooRowIdx[d], dev_cooColIdx[d],
+                dev_csrVal[d], dev_csrRowPtr[d], dev_csrColIdx[d]);
     // checkCudaErrors(cudaDeviceSynchronize());
     // print_vec_gpu(dev_csrVal[d], dev_nnz[d], "dev_csrVal"+to_string(d));
     // print_vec_gpu(dev_csrRowPtr[d], dev_m[d]+1, "dev_csrRowPtr"+to_string(d));
